e1b3173446d525b092fef0415be1872b5dd593e6
nanikit <nanikit4@gmail.com>
2021-08-14 16:09:11 +0900

    test: remove unused

--- Rengex.Test/EzTransXpTest.cs
+++ Rengex.Test/EzTransXpTest.cs
@@ -1,48 +1,9 @@
-using Rengex.Translator;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Rengex.Tests {
-  public class EztransXpTest {
-    readonly EztransXp trans;
-
-    public EztransXpTest() {
-      trans = EztransXp.Create().GetAwaiter().GetResult();
-    }
-
-    private void TestPreservation(string str) {
-      Task<string> t = trans.Translate(str);
-      t.Wait();
-      Assert.Equal(str, t.Result);
-    }
-
+  public class RengexTest {
     [Fact]
-    public void SymbolPreservationTest() {
-      TestPreservation("-----");
-      TestPreservation("#####");
-      TestPreservation("〞〞〞〞〞");
-      TestPreservation("式式式式式");
-      TestPreservation("--##〞〞@@--");
-    }
-
-    [Fact]
-    public void WhitespacePreservationTest1() {
-      TestPreservation("\r");
-    }
-
-    [Fact]
-    public void WhitespacePreservationTest2() {
-      TestPreservation("\n\nd");
-    }
-
-    [Fact]
-    public void WhitespacePreservationTest3() {
-      TestPreservation("\r\n");
-    }
-
-    [Fact]
-    public void WhitespacePreservationTest4() {
-      TestPreservation("\n\n\n ﹛\n\n");
+    public void TestStub() {
     }
   }
 }
