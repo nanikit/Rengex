45aace0d66e3ed51254fb9563521a56841cd477f
nanikit <nanikit4@gmail.com>
2021-08-30 00:05:22 +0900

    feat!: remove group renaming in debug window

--- Rengex/View/DebugWindow.xaml.cs
+++ Rengex/View/DebugWindow.xaml.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Windows;
+﻿namespace Rengex {
+  using System;
+  using System.Collections.Generic;
+  using System.Diagnostics;
+  using System.IO;
+  using System.Linq;
+  using System.Text.RegularExpressions;
+  using System.Windows;
 
-namespace Rengex {
-
-  class ConfigTabItemVM {
-    private static readonly Regex LookbehindGroup = ExtendedMatcher.GetExtendedGroupRegex("<[=!]");
-    private static readonly Regex NamedGroup = ExtendedMatcher.GetExtendedGroupRegex(@"<(\p{L}+?)>");
+  internal class ConfigTabItemVM {
+    private static readonly Regex LookbehindGroup = ExtendedMatcher.GetExtendedGroupRegex(@"\(<[=!]");
 
     private static string ConvertToRegex101Regex(string pattern) {
       string txt = pattern;
@@ -19,15 +17,13 @@ namespace Rengex {
       txt = Regex.Replace(txt, @"{0,\d{3,9}}", "*");
       txt = Regex.Replace(txt, @"{1,\d{3,9}}", "+");
       txt = Regex.Replace(txt, @"\\u(....)", @"\x{$1}");
+
       for (int idx = 0; LookbehindGroup.Match(txt, idx, out Match m); idx = m.Index + 1) {
         if (m.Groups[1].Value.Contains("*")) {
           txt = txt.Remove(m.Index) + txt[(m.Index + m.Length)..];
         }
       }
-      for (int idx = 0, seq = 0; NamedGroup.Match(txt, idx, out Match m); idx = m.Index + 1) {
-        string rep = m.Result($"(?<${{1}}{seq++}>$2)");
-        txt = $"{txt[..m.Index]}{rep}{txt[(m.Index + m.Length)..]}";
-      }
+
       string jp = Regex.Replace(TextUtils.ClassJap, @"\\u(....)", @"\x{$1}");
       txt = txt.Replace("\\jp", jp);
       txt = txt.Replace("\\w", @"[\pL\pN_]");
@@ -37,17 +33,9 @@ namespace Rengex {
 
     public string RegionPath { get; private set; }
 
-    public string Title {
-      get {
-        return Path.GetFileName(RegionPath ?? "");
-      }
-    }
+    public string Title => Path.GetFileName(RegionPath ?? "");
 
-    public string Content {
-      get {
-        return File.Exists(RegionPath) ? ConvertToRegex101Regex(File.ReadAllText(RegionPath)) : "";
-      }
-    }
+    public string Content => File.Exists(RegionPath) ? ConvertToRegex101Regex(File.ReadAllText(RegionPath)) : "";
 
     public ConfigTabItemVM(string path) {
       RegionPath = path;
@@ -77,16 +65,11 @@ namespace Rengex {
       List<ConfigTabItemVM> vms = RegexDot.RegionPaths.Select(x => new ConfigTabItemVM(x)).ToList();
       TcConfig.DataContext = vms;
 
-      if (prevIdx != -1) {
-        TcConfig.SelectedIndex = Math.Min(prevIdx, vms.Count - 1);
-      }
-      else {
-        TcConfig.SelectedIndex = 0;
-      }
+      TcConfig.SelectedIndex = prevIdx != -1 ? Math.Min(prevIdx, vms.Count - 1) : 0;
     }
 
     private void OnRegex101Click(object sender, RoutedEventArgs e) {
-      Process.Start(new ProcessStartInfo("explorer.exe", "https://regex101.com/r/dqgl16/1"));
+      _ = Process.Start(new ProcessStartInfo("explorer.exe", "https://regex101.com/r/dqgl16/1"));
     }
   }
 }
